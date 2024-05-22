4a7002633d9ed350896c5ccb273606492372b8ec
karangoyal03 <goyalkaran31@gmail.com>
2024-05-22 00:23:40 -0400

    changes for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import TOC from ".//Labs/TOC";
 import Lab2 from ".//Labs/Lab2";
 import Lab3 from ".//Labs/Lab3";
 import Kanbas from ".//Kanbas";
-
 import Labs from "./Labs";
+import LandingPage from "./LandingPage";
 
 function App() {
   return (
     <HashRouter>
       <div className="App">
-        <TOC />
+        {/* <TOC /> */}
         <Routes>
-          {/* <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="Lab1" element={<Lab1 />} />
           <Route path="Lab2" element={<Lab2 />} />
-          <Route path="Lab3" element={<Lab3 />} /> */}
+          <Route path="Lab3" element={<Lab3 />} /> 
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
---
+++ src/LandingPage/index.tsx
@@ -0,0 +1,20 @@
+export default function LandingPage() {
+  return (
+    <div>
+        <h3>Section - WEBDEV Summer Full 2024</h3>
+        <h3>Karan Goyal</h3>
+      <ul id="wd-landing-page">
+        <li>
+          <a id="wd-lab-link" href="#/Lab1">
+            Labs
+          </a>
+        </li>
+        <li>
+          <a id="wd-kanbas-link" href="#/Kanbas/Courses/1234/Home">
+            Kanbas
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
